66dc335df0241cb0bb30da006815ccd29b74aecc
João Moreira <joao.marques@levty.com>
2023-05-12 13:12:00 -0300

    Create models

--- instalelt/core/models.py
+++ instalelt/core/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
+
+class Base(models.Model):
+    created_at = models.DateField('Created', auto_now_add=True)
+    updated_at = models.DateField('Updated', auto_now=True)
+    is_active = models.BooleanField('Active', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Challenge(Base):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    issues = models.ManyToManyField("Issue")
+    result = models.FloatField()
+
+class Issue(Base):
+    statement = models.CharField('Statement', max_length=500)
+    image = models.ImageField(upload_to="issues_images", height_field=124, width_field=124)
+    right_answer = ArrayField(models.FloatField())
 
 
 class User(AbstractUser):
