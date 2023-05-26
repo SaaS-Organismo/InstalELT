cab3a9df2818705d603e92f540d3b65798322fae
Lucas <lucasmoraes7551@gmail.com>
2023-05-25 22:11:39 -0300

    Add templates, urls and views to challenge

--- instalelt/core/urls.py
+++ instalelt/core/urls.py
@@ -5,4 +5,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('novo-desafio', views.new_challenge, name="new-challenge"),
+    path('desafio/<int:challenge_id>', views.challenge_detail, name="challenge-detail")
+
 ]
--- instalelt/core/views.py
+++ instalelt/core/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'core/pages/home.html')
+
+
+def new_challenge(request):
+    if request.method == "POST":
+        print(request.POST)
+        nickname = request.POST.get("nickname", None)
+        return redirect(reverse("core:challenge-detail", kwargs={"challenge_id": 1}))
+
+    elif request.method == "GET":
+        return render(request, 'core/pages/new-challenge.html')
+
+
+def challenge_detail(request, challenge_id=None):
+    question = int(request.GET.get("question", 1))
+    ctx = {
+        "current_question": question,
+        "previous_question": question - 1,
+        "next_question": question + 1,
+    }
+    print(ctx)
+    return render(request, 'core/pages/challenge-detail.html', ctx)
+
+
+def challenge_result(request, pk):
+    return render(request, 'core/pages/challenge-result.html')
